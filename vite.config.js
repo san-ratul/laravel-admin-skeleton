322113a5f00dd4984f82cca9d09f7f5497c9f918
Saleh Ahammed Noor <salehahammednoor@gmail.com>
2026-07-14 22:15:32 +0600

    Setup React 19, Inertia.js, Vite and Tailwind CSS 4

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
 import { bunny } from 'laravel-vite-plugin/fonts';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.jsx'],
             refresh: true,
             fonts: [
                 bunny('Instrument Sans', {
@@ -14,8 +15,12 @@ export default defineConfig({
                 }),
             ],
         }),
+
+        react(),
+
         tailwindcss(),
     ],
+
     server: {
         watch: {
             ignored: ['**/storage/framework/views/**'],
